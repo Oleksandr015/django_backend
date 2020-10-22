64c6792be31a22251cfb5c57cf568063068cb74b
Oleksandr <iskander@kursGit.pl>
2020-10-22 19:22:51 +0200

    create two new models

--- django_movies/films_dict/admin.py
+++ django_movies/films_dict/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from django_movies.films_dict.models import Movie
+from films_dict.models import Movie
 
-admin.films_dict.register(Movie)
+admin.site.register(Movie)
 
 # Register your models here.
+
--- django_movies/films_dict/models.py
+++ django_movies/films_dict/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    age_limit = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=50, unique=True)
+    second_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} from {self.second_name}"
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(
@@ -10,7 +26,11 @@ class Movie(models.Model):
     released = models.DateField()
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.DO_NOTHING)
+    director = models.ForeignKey(Director, null=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f"{self.title} from {self.released}"
+
+
 
