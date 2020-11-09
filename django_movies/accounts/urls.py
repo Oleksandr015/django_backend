c5a62928742ee61bf881debf947527af53200126
Oleksandr <iskander@kursGit.pl>
2020-11-09 18:57:21 +0100

    Add changes in accounts, base.html, settings.py

--- django_movies/accounts/urls.py
+++ django_movies/accounts/urls.py
@@ -2,8 +2,14 @@ from sys import path
 
 from django.contrib.auth.views import LoginView
 
+from accounts.views import SuccessMessagedLogoutView, SubmittablePasswordChangeView
+
+
+
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', SuccessMessagedLogoutView.as_view(), name='logout'),
+    path('login/', SubmittablePasswordChangeView(), name='password_change'),
 ]
--- django_movies/django_movies/settings.py
+++ django_movies/django_movies/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'logout'
